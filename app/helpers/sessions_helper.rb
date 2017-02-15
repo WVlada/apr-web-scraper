fb756d48e50781d0f9e5f33e2cca199344a4efb8
Vladimir Bukvić <vladimir.bukvic@live.com>
2017-02-15 13:44:43 +0000

    change id to email

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,11 +3,11 @@ module SessionsHelper
   
     # Logs in the given user.
   def log_in(user)
-    session[:user_id] = user.id
+    session[:user_email] = user.email
   end
   
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    @current_user ||= User.find_by(email: session[:user_email])
   end
   
    # Returns true if the user is logged in, false otherwise.
@@ -16,7 +16,7 @@ module SessionsHelper
   end
   
   def log_out
-    session.delete(:user_id)
+    session.delete(:user_email)
     @current_user = nil
   end
 end
